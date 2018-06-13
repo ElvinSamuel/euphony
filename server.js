6d186356b39b390da80bc1707de6d9a17e5f2550
Elvin Samuel <elvinsamuel1@gmail.com>
2018-06-13 16:46:28 +0000

    Added an HTTP Header to the server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 // Basic NodeJS Server
 http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('Hello World!'); // just checking that it works
 	res.end();
 }).listen(process.env.PORT);
