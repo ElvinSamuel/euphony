56e038f116d2f9109689581ee46887de0596b5cc
Elvin Samuel <elvinsamuel1@gmail.com>
2018-06-17 19:34:26 +0000

    Got tired of it so I installed Express and got the app up and running with basic routing.

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-var http = require('http');
-var fs = require('fs'); // basic file server
+var express = require('express'),
+    app     = express();
 
+app.use(express.static(__dirname + '/public'));
 
-// Basic NodeJS Server
-http.createServer(function(req, res) {
-	fs.readFile('testFile.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data); // serving that test file 
-		res.end();
-	});
-}).listen(process.env.PORT, function(){
-	console.log(`Euphony App Up And Running On Port ${process.env.PORT}`);	
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log(`Eupony App Listening On Port ${process.env.PORT}`);
 });
