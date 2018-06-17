4cf10f5d384c1dff29de00bbf3a89c9bc45a08fd
Elvin Samuel <elvinsamuel1@gmail.com>
2018-06-17 19:49:07 +0000

    Express app fully running, with directories specified, a simplified routing system and even styling working.

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/public'));
 
+//====================================================
+// Keeping my routes in a separate folder for cleanliness
+var myRoutes = require('./routes/index');
+app.use(myRoutes);
+//=====================================================
+
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log(`Eupony App Listening On Port ${process.env.PORT}`);
 });
